83e8470767756f2dd179d3cacd72583ac0677041
wql <wqlxh7891@163.com>
2025-01-11 20:20:20 +0800

    first commit numpy

---
+++ numpy/testDemo/demo.py
@@ -0,0 +1,6 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+fig, ax = plt.subplots()             # Create a figure containing a single Axes.
+ax.plot([1, 2, 3, 4], [1, 4, 2, 3])  # Plot some data on the Axes.
+plt.show()                           # Show the figure.
